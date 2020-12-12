2387d2871ce1893de5913dd71a0d04d9f7182700
oakka <soeoakka@gmail.com>
2020-12-13 00:26:11 +0630

    first commit

---
+++ src/test/java/com/lesson/oakka/GitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesson.oakka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
